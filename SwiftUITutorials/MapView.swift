9c2a71ca63baef8f748eb4a6bb0d77a60b463367
Peter Young <young40@gmail.com>
2019-07-15 23:48:56 +0800

    update

--- SwiftUITutorials/MapView.swift
+++ SwiftUITutorials/MapView.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2019 Shiling Yang. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 39.011286, longitude: 115.166868
+        )
+
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        view.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MapView_Preview: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
