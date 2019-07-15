b4f346a819422b68dbc27156666f318ce3e4930f
Peter Young <young40@gmail.com>
2019-07-15 21:45:31 +0800

    update

---
+++ SwiftUITutorials/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorials
+//
+//  Created by Shiling Yang on 2019/7/15.
+//  Copyright © 2019 Shiling Yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Preview: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
