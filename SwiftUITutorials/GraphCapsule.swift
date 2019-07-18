b4f4225a3b6e912841e78fc4a72de50f3f52f9a4
Peter Young <young40@gmail.com>
2019-07-18 23:46:21 +0800

    update

--- SwiftUITutorials/GraphCapsule.swift
+++ SwiftUITutorials/GraphCapsule.swift
@@ -21,11 +21,18 @@ struct GraphCapsule: View {
         Length((range.lowerBound - overallRange.lowerBound) / magnitude(of: overallRange))
     }
     
+    var animation: Animation {
+        Animation.spring(initialVelocity: 5)
+            .speed(2)
+            .delay(0.03 * Double(index))
+    }
+    
     var body: some View {
         Capsule()
             .fill(Color.white)
             .frame(height: height * heightRatio, alignment: .bottom)
             .offset(x: 0, y: height * -offsetRatio)
+            .animation(animation)
     }
 }
 
--- SwiftUITutorials/HikView.swift
+++ SwiftUITutorials/HikView.swift
@@ -22,7 +22,7 @@ extension AnyTransition {
 
 struct HikView : View {
     var hike: Hike
-    @State private var showDetail = false
+    @State private var showDetail = true
     
     var body: some View {
         VStack {
