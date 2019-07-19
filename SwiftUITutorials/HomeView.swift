852268af3a89f89579c72a8cdbc5a88004952279
Peter Young <young40@gmail.com>
2019-07-19 20:08:32 +0800

    up

---
+++ SwiftUITutorials/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  SwiftUITutorials
+//
+//  Created by Shiling Yang on 2019/7/19.
+//  Copyright © 2019 Shiling Yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
+#endif
--- SwiftUITutorials/SceneDelegate.swift
+++ SwiftUITutorials/SceneDelegate.swift
@@ -23,8 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(
-                rootView: LandMarkList()
-                    .environmentObject(UserData())
+                rootView: HomeView()
             )
             self.window = window
             window.makeKeyAndVisible()
